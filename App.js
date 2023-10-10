beded55c97a3f5daebcae5ee019d5d836ec4c669
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2023-10-10 20:15:02 +0200

    image

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
+
+
+
 
 export default function App() {
+  const [mynumber, setMynumber] = useState(0);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./98497-84.png')} /> 
+
+
+      <Text>{mynumber}</Text>
+       <Button title='Plus' onPress={() => {
+       setMynumber  (mynumber +1);
+       }}/>
+
+
+
       <StatusBar style="auto" />
     </View>
   );
