bad0b7c71214e95b48749901488090406cbb5d5d
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2023-10-11 20:16:54 +0200

    list and add button

--- App.js
+++ App.js
@@ -1,22 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 
 
 export default function App() {
+
+ const [addtodo, setAddtodo] = useState("");
+ const [todoitems, setTodoitems] = useState([ {"key":"A"}, {"key":"B"} ]);
+ const [errormessage, setErrormessage] = useState("fel fel fel");
+
+ function addTothelist () {
+   if (addtodo!=""){  
+     const newtodo = todoitems. concat ({"key": addtodo});
+      setTodoitems (newtodo) ;
+    setErrormessage("");}
+      else {
+          setErrormessage("You need to enter something!");
+      }
+ }
+
+
+
   const [mynumber, setMynumber] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image source={require('./98497-84.png')} /> 
+      <Text>ToDo</Text>
+      
+      {errormessage != "" &&
+        <Text>{errormessage}</Text>
+       }
 
-
-      <Text>{mynumber}</Text>
-       <Button title='Plus' onPress={() => {
-       setMynumber  (mynumber +1);
-       }}/>
+      <TextInput value={addtodo} onChangeText={setAddtodo} placeholder='Add todo'/>
+     
+     <Button title='Add' onPress={() =>{
+      addTothelist();
+     }}/>
+     <FlatList 
+     data={todoitems}
+     renderItem = {({item}) => 
+      <Text>{item.key}</Text>
+    }
+      />
 
 
 
@@ -28,8 +54,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#00ffff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding:100,
   },
 });
+
+
+{/* <Image source={require('./98497-84.png')} /> 
+
+
+<Text>{mynumber}</Text>
+ <Button title='Plus' onPress={() => {
+ setMynumber  (mynumber +1);
+ }}/>
+ 
+ <Text>{ mynumber < 10 ? "Liten siffra" : "Stor siffra" }</Text> 
+ 
+ {mynumber > 5 &&
+  <View>
+    <Text>Mer än fem</Text>
+    <Text>mer text</Text>
+  </View>
+  } */}
